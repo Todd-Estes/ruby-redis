67737f793afc1cc41e482fcca975236d3beca8b2
todd-estes <elestes@gmail.com>
2024-08-01 12:36:31 -0400

    Add exits spec

--- test/unit/server_info_stats_test.rb
+++ test/unit/server_info_stats_test.rb
@@ -1,10 +1,14 @@
 require "minitest/autorun"
 require "minitest/pride"
-require "./app/store_object"
+require "./app/server_info_stats"
 
 class ServerInfoStatsTest < Minitest::Test 
   def setup
     options = {"role": "master"}
     @stats = ServerInfoStats.new(options)
+  end
+
+  def test_it_exists
+    assert_instance_of ServerInfoStats, @stats
   end
 end
