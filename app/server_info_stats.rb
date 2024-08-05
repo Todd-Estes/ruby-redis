3484301617dc4f2fa45c747958c01c8196f114e3
todd-estes <elestes@gmail.com>
2024-08-05 17:52:04 -0400

    Add master_replid and master_repl_offset attrs; add getters

--- app/server_info_stats.rb
+++ app/server_info_stats.rb
@@ -1,9 +1,28 @@
+require 'securerandom'
+
 class ServerInfoStats
   def initialize(options)
     @role = options[:role]
+    @master_replid = nil
+    @master_repl_offset = "0"
   end
 
   def get_role
     @role ? @role : "master"
+  end
+
+  def get_master_replid
+    @master_replid ? @master_replid : generate_replid
+  end
+
+  def get_master_repl_offset
+    @master_repl_offset
+  end    
+
+  private
+
+  def generate_replid
+    @master_replid = SecureRandom.hex(40)
+    return @master_replid
   end
 end
