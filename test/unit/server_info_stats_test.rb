86f3405630108b961732f743d76e93d431f91544
todd-estes <elestes@gmail.com>
2024-08-08 18:15:42 -0400

    Add #get_rdb test

--- test/unit/server_info_stats_test.rb
+++ test/unit/server_info_stats_test.rb
@@ -21,6 +21,11 @@ class ServerInfoStatsTest < Minitest::Test
   end
 
   def test_it_gets_master_repl_offset
-    assert_equal "0", @stats.get_master_repl_offset
+    assert_equal "0", @stats1.get_master_repl_offset
+  end
+
+  def test_it_returns_empty_rdb_file
+    rdb = ["524544495330303131fa0972656469732d76657205372e322e30fa0a72656469732d62697473c040fa056374696d65c26d08bc65fa08757365642d6d656dc2b0c41000fa08616f662d62617365c000fff06e3bfec0ff5aa2"].pack('H*')
+    assert_equal rdb, @stats1.get_rdb
   end
 end
